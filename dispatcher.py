ddcdd8bf56a779d688b0696b7b476df0a2b93d12
Ofek <ofekwit@gmail.com>
2015-08-15 21:21:36 -0500

    added locks

--- dispatcher.py
+++ dispatcher.py
@@ -40,40 +40,47 @@ class Dispatcher():
     def dispatch_next_process(self):
         """Dispatch the process at the top of the stack."""
         if self._running_process_stack:
-            active_process = self._running_process_stack[len(self._running_process_stack)-1]
-            active_process.state = State.runnable
-            if active_process.is_alive():
-                active_process.block_event.set()
-                active_process.block_event.clear()
-            else:  # has not been started before
-                active_process.start()
+            self.lock.acquire()
+            try:
+                active_process = self._running_process_stack[len(self._running_process_stack)-1]
+                active_process.state = State.runnable
+                if active_process.is_alive():
+                    active_process.block_event.set()
+                    active_process.block_event.clear()
+                else:  # has not been started before
+                    active_process.start()
 
-        if len(self._running_process_stack) > 1:
-            proc_two = self._running_process_stack[len(self._running_process_stack)-2]
-            proc_two.state = State.runnable
-            if proc_two.is_alive():
-                proc_two.block_event.set()
-                proc_two.block_event.clear()
 
-        if len(self._running_process_stack) > 2:
-            proc_three = self._running_process_stack[len(self._running_process_stack)-3]
-            proc_three.state = State.waiting
+                if len(self._running_process_stack) > 1:
+                    proc_two = self._running_process_stack[len(self._running_process_stack)-2]
+                    proc_two.state = State.runnable
+                    if proc_two.is_alive():
+                        proc_two.block_event.set()
+                        proc_two.block_event.clear()
+
+                if len(self._running_process_stack) > 2:
+                    proc_three = self._running_process_stack[len(self._running_process_stack)-3]
+                    proc_three.state = State.waiting
+            finally:
+                self.lock.release()
 
     def to_top(self, process):
         """Move the process to the top of the stack."""
-        # for i in range(len(self._running_process_stack)-1):
-        #     if self._running_process_stack[i] == process:
-        self._running_process_stack.remove(process)
-        self.io_sys.remove_window_from_process(process)
-        self.update_running_stack()
-        self._running_process_stack.append(process)
-        process.state = State.runnable
-        self.io_sys.allocate_window_to_process(process, len(self._running_process_stack)-1)
+        self.lock.acquire()
+        try:
+            self._running_process_stack.remove(process)
 
-        self.dispatch_next_process()
+            self.io_sys.remove_window_from_process(process) #known issue, we need to remove the window otherwise the process number disapears if we simply move it
+            self.update_running_stack()
+            self._running_process_stack.append(process)
+        finally:
+            self.lock.release()
+            process.state = State.runnable
+            self.io_sys.allocate_window_to_process(process, len(self._running_process_stack)-1)
+            if len(self._running_process_stack) > 2:
+                self._running_process_stack[len(self._running_process_stack)-3].state = State.waiting  # if stack is more than 2
+            self.dispatch_next_process()
 
-        if len(self._running_process_stack) > 2:
-            self._running_process_stack[len(self._running_process_stack)-3].state = State.waiting  # if stack is more than 2
 
     def update_running_stack(self):
         for i in range(0, len(self._running_process_stack)):
@@ -114,16 +121,23 @@ class Dispatcher():
         Only called from running processes.
         """
         process.iosys.remove_window_from_process(process)
-        self._running_process_stack.remove(process)
-        self.dispatch_next_process()
+        self.lock.acquire()
+        try:
+            self._running_process_stack.remove(process)
+        finally:
+            self.lock.release()
+            self.dispatch_next_process()
 
     def proc_waiting(self, process):
         """Receive notification that process is waiting for input."""
         process.state = State.waiting
-        self._running_process_stack.remove(process)
-        self._waiting_process_stack.append(process)
+        self.lock.acquire()
+        try:
+            self._running_process_stack.remove(process)
+            self._waiting_process_stack.append(process)
+        finally:
+            self.lock.release()
 
-        # self.io_sys.move_process(process, len(self._waiting_process_stack)-1)
         for i in range(0, 8):
             if not self._location_list[i]:
                 self.io_sys.move_process(process, i)
@@ -133,36 +147,42 @@ class Dispatcher():
         self.dispatch_next_process()
 
     def proc_resume(self, process):
+
         for i in range(0, 8):
             if self._location_list[i] == process:
                 self._location_list[i] = False
                 break
 
         process.state = State.runnable
-        self._waiting_process_stack.remove(process)
-        self._running_process_stack.append(process)
-        self.io_sys.move_process(process, len(self._running_process_stack)-1)
-        self.dispatch_next_process()
+        self.lock.acquire()
+        try:
+            self._waiting_process_stack.remove(process)
+            self._running_process_stack.append(process)
+        finally:
+            self.lock.release()
+            self.io_sys.move_process(process, len(self._running_process_stack)-1)
+            self.dispatch_next_process()
 
     def proc_kill(self, process):
 
         process.state = State.killed
         process.block_event.set()
-        if process.type == Type.background:
-            self._running_process_stack.remove(process)
-            self.io_sys.remove_window_from_process(process)
-            self.update_running_stack()
-        else:
-            self._waiting_process_stack.remove(process)
-            self.io_sys.remove_window_from_process(process)
-            for i in range(0, 8):
-                if self._location_list[i] == process:
-                    self._location_list[i] = False
-                    break
-
-
-
-        self.dispatch_next_process()
+        self.lock.acquire()
+        try:
+            if process.type == Type.background:
+                self._running_process_stack.remove(process)
+                self.io_sys.remove_window_from_process(process)
+                self.update_running_stack()
+            else:
+                self._waiting_process_stack.remove(process)
+                self.io_sys.remove_window_from_process(process)
+                for i in range(0, 8):
+                    if self._location_list[i] == process:
+                        self._location_list[i] = False
+                        break
+        finally:
+            self.lock.release()
+            self.dispatch_next_process()
 
     def process_with_id(self, id):
         """Return the process with the id."""
